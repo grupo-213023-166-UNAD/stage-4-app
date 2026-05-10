e363d7de37e75d5e8ec4b5f044bab84599863f54
Yannick Acosta <yjacostap@unadvirtual.edu.co>
2026-05-09 23:46:23 -0500

    Importación de paquetes y creación de clases abstractas por Yannick Acosta

---
+++ app.py
@@ -0,0 +1,67 @@
+# UNAD
+# CURSO: PROGRAMACIÓN
+# TUTOR: JHON HAROLD PATIñO PANTOJA
+# GRUPO: 213023_166
+# INTEGRANTES: MARIA STEFFANY ORTEGA CORTES Y YANNICK JOSE ACOSTA PRADO
+
+import logging
+from abc import ABC, abstractmethod
+
+# Archivo de logs
+logging.basicConfig(
+    filename='sistema_errores.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+def registrar(nivel, mensaje):
+# Carga del log y muestra en consola
+    getattr(logging, nivel)(mensaje)
+    print(f"  [{nivel.upper()}] {mensaje}")
+
+# Manejo de errores personalizados
+class SistemaError(Exception):
+    pass
+
+class DatosInvalidosError(SistemaError):
+    pass
+
+class ServicioNoDisponibleError(SistemaError):
+    pass
+
+class ReservaError(SistemaError):
+    pass
+
+
+# Clase abstracta
+class Entidad(ABC):
+    @abstractmethod
+    def mostrar_info(self):
+        pass
+
+    @abstractmethod
+    def validar(self):
+        pass
+
+
+# Clase abstracta de cliente
+class Cliente(Entidad):
+    def __init__(self, id_cliente, nombre, email, telefono):
+        self.id_cliente = id_cliente
+        # Validaciones
+        if not nombre or len(nombre.strip()) < 3:
+            raise DatosInvalidosError(f"Nombre invalido: '{nombre}'.")
+        if "@" not in email or "." not in email.split("@")[-1]:
+            raise DatosInvalidosError(f"Email invalido: '{email}'.")
+        if not str(telefono).replace("-", "").isdigit() or len(str(telefono)) < 7:
+            raise DatosInvalidosError(f"Teléfono invalido: '{telefono}'.")
+        self.nombre   = nombre.strip()
+        self.email    = email.strip().lower()
+        self.telefono = str(telefono)
+        self.reservas = []
+
+    def validar(self):
+        return bool(self.nombre and self.email and self.telefono)
+
+    def mostrar_info(self):
+        return f"Cliente [{self.id_cliente}] {self.nombre} | {self.email} | {self.telefono}"
